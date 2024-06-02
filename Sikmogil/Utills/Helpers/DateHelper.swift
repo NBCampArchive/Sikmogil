12b5f6bd92fbce7e1bb42221b843fcc6c6b59066
HyunRyeol Park <devpark435@gmail.com>
2024-06-02 23:43:39 +0900

    feat #2 - Add DateFormatter Helper Class

--- Sikmogil/Utills/Helpers/DateHelper.swift
+++ Sikmogil/Utills/Helpers/DateHelper.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+class DateHelper {
+
+    static let shared = DateHelper()
+
+    private init() {}
+
+    // "yyyy.MM.dd" 형식으로 변환하는 함수
+    func formatDateToYearMonthDay(_ date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy.MM.dd"
+        return dateFormatter.string(from: date)
+    }
+
+    // "yy.MM.dd.HH:mm" 형식으로 변환하는 함수
+    func formatDateToYearMonthDayHourMinute(_ date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yy.MM.dd.HH:mm"
+        return dateFormatter.string(from: date)
+    }
+    
+    // 서버에서 받은 문자열을 Date로 변환 (예제: "2024-06-02T14:30:00Z")
+    func dateFromServerString(_ dateString: String) -> Date? {
+        let dateFormatter = ISO8601DateFormatter()
+        return dateFormatter.date(from: dateString)
+    }
+}
+
