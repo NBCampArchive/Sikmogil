cb67a7b3244d99b67ab0e9c6f0792e17b642cbf6
HyunRyeol Park <devpark435@gmail.com>
2024-06-03 00:51:41 +0900

    feat #3 - 커스텀 ProgressBar Test

--- Sikmogil/Presentation/Example/View/ViewController.swift
+++ Sikmogil/Presentation/Example/View/ViewController.swift
@@ -6,14 +6,53 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class ViewController: UIViewController {
-
+    
+    let circularProgressBar = CircularProgressBar().then {
+        $0.backgroundColor = .clear
+        $0.progressColor = .systemBlue
+        $0.trackColor = .lightGray
+    }
+    
+    let customCircularProgressBar = CustomCircularProgressBar().then {
+        $0.backgroundColor = .clear
+        $0.progressColor = .systemYellow
+        $0.trackColor = .lightGray
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .red
+        
+        view.backgroundColor = .white
+        
+        setupViews()
+        setupConstraints()
+        
+        // 프로그래스 업데이트 (예시: 0.75 -> 75%)
+        circularProgressBar.progress = 0.95
+        customCircularProgressBar.progress = 0.9
     }
+    
+    private func setupViews() {
+        view.addSubviews(circularProgressBar, customCircularProgressBar)
+    }
+    
+    private func setupConstraints() {
+        circularProgressBar.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.centerX.equalToSuperview()
+            $0.width.height.equalTo(300)
+        }
+        customCircularProgressBar.snp.makeConstraints {
+            $0.top.equalTo(circularProgressBar.snp.bottom).offset(20)
+            $0.centerX.equalToSuperview()
+            $0.width.height.equalTo(300)
+        }
+    }
+    
 }
 
 // ViewController나 View 파일은 각 뷰 폴더의 하위 파일에 View라는 파일을 생성해 추가해주세요.
