d53b6316bf9771fa998dbbedacee9638c1ef8652
정유진 <yujin3504@gmail.com>
2024-06-03 16:59:59 +0900

    setting #7 - tabbar VC 변경

--- Sikmogil/Presentation/TabBar/BottomTabBarController.swift
+++ Sikmogil/Presentation/TabBar/BottomTabBarController.swift
@@ -23,7 +23,7 @@ class BottomTabBarController: UITabBarController {
         let dietVC = ViewController()
         dietVC.tabBarItem = UITabBarItem(title: "식단", image: UIImage(named: "diet"), tag: 1)
         
-        let exerciseVC = ViewController()
+        let exerciseVC = ExerciseViewController()
         exerciseVC.tabBarItem = UITabBarItem(title: "운동", image: UIImage(named: "exercise"), tag: 2)
         
         let communicationVC = ViewController()
