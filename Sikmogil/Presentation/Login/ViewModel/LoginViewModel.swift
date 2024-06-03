c5d4c4d921c0207aab2f02d96dab44f077a05a4b
HyunRyeol Park <devpark435@gmail.com>
2024-06-03 20:52:30 +0900

    feat #11 - LoginViewModel Test Code

--- Sikmogil/Presentation/Login/ViewModel/LoginViewModel.swift
+++ Sikmogil/Presentation/Login/ViewModel/LoginViewModel.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+class LoginViewModel {
+    
+    // Apple 로그인
+    func signInWithApple() {
+        print("Signin with Apple")
+    }
+    
+    // Google 로그인
+    func signInWithGoogle() {
+        print("Signin with Google")
+    }
+}
