3fdc9ad56c7c673009661c10c68f300865a9bc6b
Stay-jun <112465877+Neo-agnes@users.noreply.github.com>
2024-06-03 18:49:28 +0900

    [feat]Height, Weight, Gender UI

--- Sikmogil/Presentation/Example/View/Profile/ProfileViewController.swift
+++ Sikmogil/Presentation/Example/View/Profile/ProfileViewController.swift
@@ -10,10 +10,10 @@ import SnapKit
 import Then
 
 class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
     let scrollView = UIScrollView()
     let contentView = UIView()
-
+    
     // Top bar elements
     let topBar = UIView()
     let profileLabel = UILabel().then {
@@ -23,7 +23,7 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
     let settingsButton = UIButton().then {
         $0.setImage(UIImage(systemName: "gearshape"), for: .normal)
     }
-
+    
     let profileImageView = UIImageView().then {
         $0.image = UIImage(named: "defaultProfileImage") // 임의의 프로필 이미지
         $0.contentMode = .scaleAspectFill
@@ -37,7 +37,7 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
         $0.textColor = .white
         $0.textAlignment = .center
         $0.backgroundColor = UIColor(red: 0.0, green: 0.7, blue: 0.7, alpha: 1.0)
-        $0.layer.cornerRadius = 12
+        $0.layer.cornerRadius = 10
         $0.layer.masksToBounds = true
     }
     let nameLabel = UILabel().then {
@@ -52,35 +52,56 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
         $0.layer.shadowOffset = CGSize(width: 0, height: 1)
         $0.layer.shadowRadius = 4
     }
+    let weightTitleLabel = UILabel().then {
+        $0.text = "몸무게"
+        $0.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        $0.textColor = .darkGray
+    }
     let weightLabel = UILabel().then {
         $0.text = "0.0kg"
-        $0.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        $0.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+    }
+    let heightTitleLabel = UILabel().then {
+        $0.text = "키"
+        $0.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        $0.textColor = .darkGray
     }
     let heightLabel = UILabel().then {
         $0.text = "000cm"
-        $0.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        $0.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+    }
+    let genderTitleLabel = UILabel().then {
+        $0.text = "성별"
+        $0.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        $0.textColor = .darkGray
     }
     let genderLabel = UILabel().then {
         $0.text = "남자"
-        $0.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        $0.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+    }
+    let separator1 = UIView().then {
+        $0.backgroundColor = UIColor(named: "appDarkGray")
+    }
+    let separator2 = UIView().then {
+        $0.backgroundColor = UIColor(named: "appDarkGray")
     }
     let tableView = UITableView()
     let logoutButton = UIButton().then {
         $0.setTitle("로그아웃", for: .normal)
         $0.setTitleColor(.red, for: .normal)
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
         setupProfileImageTap()
     }
-
+    
     private func setupUI() {
         view.backgroundColor = .white
         view.addSubview(scrollView)
         scrollView.addSubview(contentView)
-
+        
         // Add top bar elements
         contentView.addSubview(topBar)
         topBar.addSubview(profileLabel)
@@ -89,14 +110,14 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
         [profileImageView, levelBadgeLabel, nameLabel, infoView, tableView, logoutButton].forEach {
             contentView.addSubview($0)
         }
-
-        [weightLabel, heightLabel, genderLabel].forEach {
+        
+        [weightTitleLabel, weightLabel, heightTitleLabel, heightLabel, genderTitleLabel, genderLabel, separator1, separator2].forEach {
             infoView.addSubview($0)
         }
         
         setupConstraints()
     }
-
+    
     private func setupConstraints() {
         scrollView.snp.makeConstraints {
             $0.edges.equalTo(view.safeAreaLayoutGuide)
@@ -107,7 +128,7 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
             $0.width.equalTo(scrollView)
             $0.height.equalTo(scrollView).priority(.low)
         }
-
+        
         // Top bar constraints
         topBar.snp.makeConstraints {
             $0.top.equalTo(contentView).offset(20)
@@ -123,108 +144,103 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
             $0.right.equalTo(topBar).offset(-16)
             $0.width.height.equalTo(24)
         }
-
+        
         profileImageView.snp.makeConstraints {
             $0.top.equalTo(topBar.snp.bottom).offset(20)
             $0.left.equalTo(contentView).offset(16)
             $0.width.height.equalTo(100)
         }
-
+        
         levelBadgeLabel.snp.makeConstraints {
-            $0.bottom.equalTo(profileImageView).offset(5)
+            $0.bottom.equalTo(profileImageView.snp.bottom).offset(-5)
             $0.centerX.equalTo(profileImageView.snp.right).offset(-10)
             $0.height.equalTo(24)
             $0.width.equalTo(50)
         }
-
+        
         nameLabel.snp.makeConstraints {
             $0.centerY.equalTo(profileImageView)
             $0.left.equalTo(profileImageView.snp.right).offset(16)
             $0.right.equalTo(contentView).offset(-16)
         }
-
+        
         infoView.snp.makeConstraints {
             $0.top.equalTo(profileImageView.snp.bottom).offset(20)
             $0.left.equalTo(contentView).offset(16)
             $0.right.equalTo(contentView).offset(-16)
             $0.height.equalTo(80)
         }
-
-        weightLabel.snp.makeConstraints {
-            $0.centerY.equalTo(infoView)
-            $0.left.equalTo(infoView).offset(16)
+        
+        weightTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(infoView).offset(16)
+            $0.left.equalTo(infoView).offset(43)
         }
-
-        heightLabel.snp.makeConstraints {
-            $0.centerY.equalTo(infoView)
+        
+        weightLabel.snp.makeConstraints {
+            $0.top.equalTo(weightTitleLabel.snp.bottom).offset(4)
+            $0.left.equalTo(infoView).offset(35)
+            $0.bottom.equalTo(infoView).offset(-16)
+        }
+        
+        heightTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(infoView).offset(16)
             $0.centerX.equalTo(infoView)
         }
-
-        genderLabel.snp.makeConstraints {
-            $0.centerY.equalTo(infoView)
-            $0.right.equalTo(infoView).offset(-16)
+        
+        heightLabel.snp.makeConstraints {
+            $0.top.equalTo(heightTitleLabel.snp.bottom).offset(4)
+            $0.centerX.equalTo(infoView)
+            $0.bottom.equalTo(infoView).offset(-16)
         }
-
+        
+        genderTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(infoView).offset(16)
+            $0.right.equalTo(infoView).offset(-43)
+        }
+        
+        genderLabel.snp.makeConstraints {
+            $0.top.equalTo(genderTitleLabel.snp.bottom).offset(4)
+            $0.right.equalTo(infoView).offset(-35)
+            $0.bottom.equalTo(infoView).offset(-16)
+        }
+        
+        separator1.snp.makeConstraints { make in
+            make.centerX.equalTo(infoView.snp.left).offset(119.33)
+            make.top.equalTo(infoView).offset(16)
+            make.bottom.equalTo(infoView).offset(-16)
+            make.width.equalTo(1)
+        }
+        
+        separator2.snp.makeConstraints { make in
+            make.centerX.equalTo(infoView.snp.left).offset(119.33 * 2)
+            make.top.equalTo(infoView).offset(16)
+            make.bottom.equalTo(infoView).offset(-16)
+            make.width.equalTo(1)
+        }
+        
         tableView.snp.makeConstraints {
             $0.top.equalTo(infoView.snp.bottom).offset(20)
             $0.left.equalTo(contentView).offset(16)
             $0.right.equalTo(contentView).offset(-16)
             $0.height.equalTo(200) // 임의의 높이, 실제 데이터에 따라 조정 필요
         }
-
+        
         logoutButton.snp.makeConstraints {
             $0.top.equalTo(tableView.snp.bottom).offset(20)
             $0.centerX.equalTo(contentView)
             $0.bottom.equalTo(contentView).offset(-20)
         }
     }
-
+    
     private func setupProfileImageTap() {
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(profileImageTapped))
         profileImageView.addGestureRecognizer(tapGestureRecognizer)
     }
-
+    
     @objc private func profileImageTapped() {
         let imagePickerController = UIImagePickerController()
         imagePickerController.delegate = self
         imagePickerController.sourceType = .photoLibrary
         present(imagePickerController, animated: true, completion: nil)
-    }
-
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let selectedImage = info[.originalImage] as? UIImage {
-            profileImageView.image = selectedImage
-        }
-        dismiss(animated: true, completion: nil)
-    }
-
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        dismiss(animated: true, completion: nil)
-    }
-}
-
-extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3 // 임의의 데이터 개수
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
-        switch indexPath.row {
-        case 0:
-            cell.textLabel?.text = "메달 확인"
-        case 1:
-            cell.textLabel?.text = "작성한 게시글"
-        case 2:
-            cell.textLabel?.text = "공감한 게시글"
-        default:
-            break
-        }
-        return cell
-    }
-
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        // 각 항목에 대한 동작 추가
     }
 }
