99ffabbc7cfadfff2df2d8290ab84fef065374be
정유진 <yujin3504@gmail.com>
2024-06-04 19:25:14 +0900

    design #7 - ExerciseTimerView 초기 구성

--- Sikmogil/Presentation/Example/View/ExerciseTimerViewController.swift
+++ Sikmogil/Presentation/Example/View/ExerciseTimerViewController.swift
@@ -8,22 +8,78 @@
 import UIKit
 
 class ExerciseTimerViewController: UIViewController {
-
+    
+    // TODO: 프로그레스 바
+    private let timeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "00 : 00"
+        label.font = Suite.semiBold.of(size: 60)
+        return label
+    }()
+    
+    
+    // TODO: 버튼 상태 변화 -> 이미지 변경, Timer
+    private let stopPauseButton: UIButton = {
+        let button = UIButton()
+        button.setImage(.pause, for: .normal)
+        return button
+    }()
+    
+    private let statusLabel: UILabel = {
+        let label = UILabel()
+        label.text = "START"
+        label.font = Suite.semiBold.of(size: 24)
+        label.textColor = .customGreen
+        return label
+    }()
+    
+    private let recordButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("기록하기", for: .normal)
+        button.titleLabel?.font = Suite.bold.of(size: 18)
+        button.backgroundColor = .customBlack
+        button.tintColor = .white
+        button.layer.cornerRadius = 16
+        return button
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupViews()
+        setupConstraints()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupViews() {
+        view.backgroundColor = .white
+        view.addSubviews(timeLabel, stopPauseButton, statusLabel, recordButton)
     }
-    */
+    
+    private func setupConstraints() {
+        timeLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(240)
+            $0.centerX.equalToSuperview()
+        }
+        
+        stopPauseButton.snp.makeConstraints {
+            $0.top.equalTo(timeLabel.snp.bottom).offset(34)
+            $0.centerX.equalToSuperview()
+        }
+        
+        statusLabel.snp.makeConstraints {
+            $0.top.equalTo(stopPauseButton.snp.bottom).offset(8)
+            $0.centerX.equalToSuperview()
+        }
+        
+        recordButton.snp.makeConstraints {
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(32)
+            $0.height.equalTo(60)
+            $0.leading.trailing.equalToSuperview().inset(16)
+        }
 
+    }
+}
+
+#Preview {
+    ExerciseTimerViewController()
 }
