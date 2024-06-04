f4f753c54e3a97ace4b04005453887cc5569c8d8
HyunRyeol Park <devpark435@gmail.com>
2024-06-04 22:11:18 +0900

    feat #11 - Add Oauth LoginManager

--- Sikmogil/Services/Network/LoginAPIManager.swift
+++ Sikmogil/Services/Network/LoginAPIManager.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import Alamofire
+import KeychainSwift
+
+class LoginAPIManager{
+    
+    static let shared = LoginAPIManager()
+    
+    let keychain = KeychainSwift()
+    
+    let baseURL = Bundle.main.baseURL
+    
+    private init() {}
+    
+    func getAccessToken(authCode: String?, provider: String, completion: @escaping (Result<LoginModel, Error>) -> Void){
+        let url = "\(baseURL)/api/\(provider)/token"
+        // Body 설정
+        let parameters: Parameters = [
+            "authCode" : authCode!,
+        ]
+        
+        AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default)
+            .validate()
+            .responseDecodable(of: LoginModel.self) { response in
+                switch response.result {
+                case .success(let tokenResponse):
+                    self.keychain.set(tokenResponse.data.accessToken, forKey: "accessToken")
+                    self.keychain.set(tokenResponse.data.refreshToken, forKey: "refreshToken")
+                    completion(.success(tokenResponse))
+                case .failure(let error):
+                    print("로그인 요청 에러")
+                    completion(.failure(error))
+                }
+            }
+    }
+}
