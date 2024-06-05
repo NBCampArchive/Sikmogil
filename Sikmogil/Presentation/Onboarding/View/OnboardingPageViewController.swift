c906fb90ee049269c945e987c0d9d7cbc18a4575
HyunRyeol Park <devpark435@gmail.com>
2024-06-05 22:28:55 +0900

    feat #11 - Edit OnboardingViewController with RxSwift

--- Sikmogil/Presentation/Onboarding/View/OnboardingPageViewController.swift
+++ Sikmogil/Presentation/Onboarding/View/OnboardingPageViewController.swift
@@ -8,121 +8,122 @@
 import UIKit
 import SnapKit
 import Then
+import RxSwift
+import RxCocoa
 
-class OnboardingPageViewController: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+class OnboardingViewController: UIViewController {
     
     let viewModel = OnboardingViewModel()
+    
+    private let disposeBag = DisposeBag()
     
     private let progressBar = OnboardingProgressBar().then {
         $0.setProgress(0.33)
     }
     
-    private let containerView = UIView()
+    private let pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal).then {
+        $0.view.backgroundColor = .clear
+    }
     
     private var currentIndex = 0
     
     private lazy var orderedViewControllers: [UIViewController] = {
-        return [Step1ViewController(),
-                Step2ViewController(),
-                Step3ViewController()]
+        let step1VC = Step1ViewController()
+        step1VC.viewModel = viewModel
+        let step2VC = Step2ViewController()
+        step2VC.viewModel = viewModel
+        let step3VC = Step3ViewController()
+        step3VC.viewModel = viewModel
+        return [step1VC, step2VC, step3VC]
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        dataSource = self
-        delegate = self
         view.backgroundColor = .white
+        setupLayout()
+        setupPageViewController()
+        bindViewModel()
         
         if let firstViewController = orderedViewControllers.first {
-            addChild(firstViewController)
-            containerView.addSubview(firstViewController.view)
-            firstViewController.view.frame = containerView.bounds
-            firstViewController.didMove(toParent: self)
+            pageViewController.setViewControllers([firstViewController], direction: .forward, animated: false, completion: nil)
         }
-        
-        setupLayout()
     }
     
     private func setupLayout() {
-        view.addSubviews(progressBar, containerView)
+        view.addSubviews(progressBar, pageViewController.view)
         
         progressBar.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide).offset(32)
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(16)
             $0.leading.equalToSuperview().offset(16)
             $0.trailing.equalToSuperview().offset(-16)
+            $0.height.equalTo(15)
         }
         
-        containerView.snp.makeConstraints {
-            $0.top.equalTo(progressBar.snp.bottom)
+        pageViewController.view.snp.makeConstraints {
+            $0.top.equalTo(progressBar.snp.bottom).offset(16)
             $0.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
         }
-        
     }
     
-    private lazy var viewControllersContainer: UIView = {
-            let containerView = UIView()
-            containerView.backgroundColor = .clear
-            addChild(self)
-            containerView.addSubview(self.view)
-            self.view.snp.makeConstraints { make in
-                make.edges.equalToSuperview()
-            }
-            self.didMove(toParent: self)
-            return containerView
-        }()
-        
-        func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-            guard let index = orderedViewControllers.firstIndex(of: viewController), index > 0 else {
-                return nil
-            }
-            return orderedViewControllers[index - 1]
-        }
-        
-        func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-            guard let index = orderedViewControllers.firstIndex(of: viewController), index < orderedViewControllers.count - 1 else {
-                return nil
-            }
-            return orderedViewControllers[index + 1]
-        }
-        
-        func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
-            if completed {
-                if let currentViewController = viewControllers?.first,
-                   let index = orderedViewControllers.firstIndex(of: currentViewController) {
-                    currentIndex = index
-                    updateProgressBar()
-                }
-            }
-        }
-        
-        private func updateProgressBar() {
-            let progress = Float(currentIndex + 1) / Float(orderedViewControllers.count)
-            progressBar.setProgress(progress)
-        }
-        
-        func moveToNextPage() {
-            if currentIndex < orderedViewControllers.count - 1 {
-                let nextIndex = currentIndex + 1
-                let nextViewController = orderedViewControllers[nextIndex]
-                
-                setViewControllers([nextViewController], direction: .forward, animated: true) { [weak self] _ in
-                    self?.updateProgressBar()
-                    self?.currentIndex = nextIndex
-                }
-            }
-        }
+    private func setupPageViewController() {
+        addChild(pageViewController)
+        pageViewController.didMove(toParent: self)
+        pageViewController.dataSource = self
+        pageViewController.delegate = self
+    }
     
+    private func updateProgressBar() {
+        let progress = Float(viewModel.currentIndex.value + 1) / Float(orderedViewControllers.count)
+        progressBar.setProgress(progress)
+    }
+    
+    private func bindViewModel() {
+        viewModel.currentIndex
+            .subscribe(onNext: { [weak self] index in
+                guard let self = self else { return }
+                self.setViewcontrollersFromIndex(index: index)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    private func setViewcontrollersFromIndex(index: Int) {
+        if index < orderedViewControllers.count {
+            let viewController = orderedViewControllers[index]
+            pageViewController.setViewControllers([viewController], direction: .forward, animated: true) { [weak self] completed in
+                guard let self = self else { return }
+                if completed {
+                    print("Page transition completed")
+                    self.updateProgressBar()
+                } else {
+                    print("Page transition not completed")
+                }
+            }
+        }
+    }
 }
 
-extension UIViewController {
-    var onboardingPageViewController: OnboardingPageViewController? {
-        var parentController = parent
-        while parentController != nil {
-            if let onboardingPageVC = parentController as? OnboardingPageViewController {
-                return onboardingPageVC
-            }
-            parentController = parentController?.parent
+extension OnboardingViewController: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let index = orderedViewControllers.firstIndex(of: viewController), index > 0 else {
+            return nil
         }
-        return nil
+        return orderedViewControllers[index - 1]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let index = orderedViewControllers.firstIndex(of: viewController), index < orderedViewControllers.count - 1 else {
+            return nil
+        }
+        return orderedViewControllers[index + 1]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        if completed {
+            if let currentViewController = pageViewController.viewControllers?.first,
+               let index = orderedViewControllers.firstIndex(of: currentViewController) {
+                viewModel.currentIndex.accept(index)
+                updateProgressBar()
+            }
+        }
     }
 }
