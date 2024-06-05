e950a79501e6eef3f8fe406be5433c3c44ffd058
HyunRyeol Park <devpark435@gmail.com>
2024-06-05 19:20:34 +0900

    feat #11 - Add Onboarding, GID ClientID

--- Sikmogil/Application/SceneDelegate.swift
+++ Sikmogil/Application/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(windowScene: windowScene)
         
-        let viewController = LoginViewController()
+        let viewController = OnboardingPageViewController()
         window?.rootViewController = viewController
         window?.makeKeyAndVisible()
     }
