1f8faf64d4c7b881f8b3a28a6f1fbf3460ad300c
HyunRyeol Park <devpark435@gmail.com>
2024-06-05 19:19:43 +0900

    feat #11 - Add Onboarding ViewModel

--- Sikmogil/Presentation/Onboarding/ViewModel/OnboardingViewModel.swift
+++ Sikmogil/Presentation/Onboarding/ViewModel/OnboardingViewModel.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+class OnboardingViewModel {
+    var userProfile = UserProfile()
+    var currentStep = 0
+    
+    func saveProfileData(nickname: String, height: String, weight: String, gender: String) {
+        userProfile.nickname = nickname
+        userProfile.height = height
+        userProfile.weight = weight
+        userProfile.gender = gender
+    }
+    
+    func saveTargetData(targetWeight: String, targetDate: Date) {
+        userProfile.targetWeight = targetWeight
+        userProfile.targetDate = targetDate
+    }
+    
+    func saveReminderData(reminderTime: String) {
+        userProfile.toDate = Date()
+        userProfile.reminderTime = reminderTime
+    }
+    
+    func submitProfile(completion: @escaping (Result<Void, Error>) -> Void) {
+        // 서버 통신 로직
+    }
+}
