3b7fee902624412b00e6e15a7df19e2f9068fa0f
Stay-jun <112465877+Neo-agnes@users.noreply.github.com>
2024-06-07 03:16:48 +0900

    [feat]알림역역 테이블 뷰

---
+++ Sikmogil/Presentation/Profile/View/AlarmTableViewCell.swift
@@ -0,0 +1,72 @@
+//
+//  AlarmTableViewCell.swift
+//  Sikmogil
+//
+//  Created by Developer_P on 6/6/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class AlarmTableViewCell: UITableViewCell {
+    
+    static let identifier = "AlarmTableViewCell"
+    
+    let containerView = UIView().then {
+        $0.backgroundColor = UIColor(named: "appLightGray")
+        $0.layer.cornerRadius = 12
+        $0.layer.masksToBounds = true
+    }
+    
+    let label = UILabel().then {
+        $0.font = Suite.regular.of(size: 16)
+    }
+    
+    let customSwitch = UISwitch().then {
+        $0.onTintColor = UIColor(named: "appSkyBlue")
+    }
+    
+    let accessoryButton = UIButton(type: .system).then {
+        $0.setImage(UIImage(systemName: "chevron.right"), for: .normal)
+        $0.tintColor = UIColor(named: "appBlack")
+    }
+    
+    var showsAccessoryButton: Bool = true {
+        didSet {
+            accessoryButton.isHidden = !showsAccessoryButton
+        }
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.addSubview(containerView)
+        containerView.addSubview(label)
+        containerView.addSubview(customSwitch)
+        containerView.addSubview(accessoryButton)
+        
+        containerView.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(8)
+        }
+        
+        label.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().offset(16)
+        }
+        
+        customSwitch.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalTo(accessoryButton.snp.leading).offset(16)
+        }
+        
+        accessoryButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().offset(-16)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ Sikmogil/Presentation/Profile/View/ReminderSettingsViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ReminderSettingsViewController.swift
+//  Sikmogil
+//
+//  Created by Developer_P on 6/6/24.
+//
+
+import Foundation
