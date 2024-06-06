047315b1a2a296e1c5ace0a133773d3d33ed9868
Stay-jun <112465877+Neo-agnes@users.noreply.github.com>
2024-06-07 03:22:20 +0900

    [feat]리마인드 뷰 컨트롤러 (임시로 넣어둠)

--- Sikmogil/Presentation/Profile/View/ReminderSettingsViewController.swift
+++ Sikmogil/Presentation/Profile/View/ReminderSettingsViewController.swift
@@ -5,4 +5,26 @@
 //  Created by Developer_P on 6/6/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
+
+class ReminderSettingsViewController: UIViewController {
+    
+    private let titleLabel = UILabel().then {
+        $0.text = "리마인드 알림 시간 설정"
+        $0.font = Suite.bold.of(size: 24)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        view.addSubview(titleLabel)
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(16)
+            $0.centerX.equalToSuperview()
+        }
+    }
+}
+
