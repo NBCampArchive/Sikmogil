e305399693e082d62a1b3e4acfdc708e56b88f4f
Stay-jun <112465877+Neo-agnes@users.noreply.github.com>
2024-06-07 21:10:32 +0900

    feat #8 - 리마인드 페이지 (임시)

--- Sikmogil/Presentation/Profile/View/ReminderSettingsViewController.swift
+++ Sikmogil/Presentation/Profile/View/ReminderSettingsViewController.swift
@@ -11,20 +11,23 @@ import Then
 
 class ReminderSettingsViewController: UIViewController {
     
+    // MARK: - 속성정의
     private let titleLabel = UILabel().then {
         $0.text = "리마인드 알림 시간 설정"
         $0.font = Suite.bold.of(size: 24)
+        $0.textColor = .appBlack
     }
     
+    // MARK: - 생명주기정의
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         view.addSubview(titleLabel)
         
+        // MARK: - 레이아웃
         titleLabel.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide).offset(16)
             $0.centerX.equalToSuperview()
         }
     }
 }
-
