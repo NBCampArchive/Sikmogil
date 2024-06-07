23dd2e4e50e7275402838219d61c6e0af0bc81f0
Stay-jun <112465877+Neo-agnes@users.noreply.github.com>
2024-06-07 21:07:20 +0900

    feat #8 - 프로필 컨트롤러 (뷰나눔) - 테이블 뷰 커뮤니티

--- Sikmogil/Presentation/Profile/View/ProfileTableView.swift
+++ Sikmogil/Presentation/Profile/View/ProfileTableView.swift
@@ -1,5 +1,5 @@
 //
-//  ProfileDetailsView.swift
+//  ProfileTableView.swift
 //  Sikmogil
 //
 //  Created by Developer_P on 6/7/24.
@@ -9,131 +9,58 @@ import UIKit
 import SnapKit
 import Then
 
-class ProfileDetailsView: UIView {
-
-    let weightStackView = createVerticalStackView()
-    let heightStackView = createVerticalStackView()
-    let genderStackView = createVerticalStackView()
-
-    let separator1 = createSeparator()
-    let separator2 = createSeparator()
-
-    let weightTitleLabel = createTitleLabel(text: "몸무게")
-    let weightLabel = createDetailLabel(text: "0.0kg")
-    let heightTitleLabel = createTitleLabel(text: "키")
-    let heightLabel = createDetailLabel(text: "000cm")
-    let genderTitleLabel = createTitleLabel(text: "성별")
-    let genderLabel = createDetailLabel(text: "남자")
-
+class ProfileTableView: UIView, UITableViewDelegate, UITableViewDataSource {
+    
+    // MARK: - 테이블 속성정의
+    let tableView = UITableView().then {
+        $0.register(ProfileTableViewCell.self, forCellReuseIdentifier: "ProfileCell")
+        $0.separatorStyle = .none // 구분선 제거
+        $0.rowHeight = 60 // 커뮤니티 각 셀별 높이 지정
+    }
+    
+    // MARK: - 초기화 메서드
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupUI()
+        setupTableView()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
+    // MARK: - UI 설정
     private func setupUI() {
-        addSubview(weightStackView)
-        addSubview(heightStackView)
-        addSubview(genderStackView)
-        addSubview(separator1)
-        addSubview(separator2)
-
-        weightStackView.addArrangedSubview(weightTitleLabel)
-        weightStackView.addArrangedSubview(weightLabel)
-
-        heightStackView.addArrangedSubview(heightTitleLabel)
-        heightStackView.addArrangedSubview(heightLabel)
-
-        genderStackView.addArrangedSubview(genderTitleLabel)
-        genderStackView.addArrangedSubview(genderLabel)
-
-        weightStackView.snp.makeConstraints {
-            $0.left.equalToSuperview().offset(16)
-            $0.centerY.equalToSuperview()
-            $0.width.equalTo(heightStackView)
-        }
-
-        heightStackView.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-            $0.centerY.equalToSuperview()
-            $0.width.equalTo(genderStackView)
-        }
-
-        genderStackView.snp.makeConstraints {
-            $0.right.equalToSuperview().offset(-16)
-            $0.centerY.equalToSuperview()
-            $0.width.equalTo(weightStackView)
-        }
-
-        separator1.snp.makeConstraints {
-            $0.centerY.equalToSuperview()
-            $0.left.equalTo(weightStackView.snp.right).offset(8)
-            $0.right.equalTo(heightStackView.snp.left).offset(-8)
-            $0.width.equalTo(1)
-            $0.height.equalTo(30)
-        }
-
-        separator2.snp.makeConstraints {
-            $0.centerY.equalToSuperview()
-            $0.left.equalTo(heightStackView.snp.right).offset(8)
-            $0.right.equalTo(genderStackView.snp.left).offset(-8)
-            $0.width.equalTo(1)
-            $0.height.equalTo(30)
-        }
-
-        weightTitleLabel.snp.makeConstraints {
-            $0.height.equalTo(30)
-        }
-
-        heightTitleLabel.snp.makeConstraints {
-            $0.height.equalTo(30)
-        }
-
-        genderTitleLabel.snp.makeConstraints {
-            $0.height.equalTo(30)
-        }
-
-        weightLabel.snp.makeConstraints {
-            $0.height.equalTo(30)
-        }
-
-        heightLabel.snp.makeConstraints {
-            $0.height.equalTo(30)
-        }
-
-        genderLabel.snp.makeConstraints {
-            $0.height.equalTo(30)
+        self.addSubview(tableView)
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
         }
     }
-
-    private static func createVerticalStackView() -> UIStackView {
-        return UIStackView().then {
-            $0.axis = .vertical
-            $0.alignment = .center
-        }
+    
+    // MARK: - 테이블뷰 설정
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-    private static func createSeparator() -> UIView {
-        return UIView().then {
-            $0.backgroundColor = .lightGray
-        }
+    
+    // MARK: - UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
     }
-
-    private static func createTitleLabel(text: String) -> UILabel {
-        return UILabel().then {
-            $0.text = text
-            $0.font = Suite.regular.of(size: 14)
-            $0.textColor = .darkGray
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileCell", for: indexPath) as? ProfileTableViewCell else {
+            return UITableViewCell()
         }
+        let titles = ["메달 확인", "작성한 게시글", "공감한 게시글"]
+        let icons = ["cup", "pencilline", "heart"]
+        cell.configure(with: titles[indexPath.row], iconName: icons[indexPath.row])
+        
+        return cell
     }
-
-    private static func createDetailLabel(text: String) -> UILabel {
-        return UILabel().then {
-            $0.text = text
-            $0.font = Suite.bold.of(size: 16)
-        }
+    
+    // MARK: - UITableViewDelegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
