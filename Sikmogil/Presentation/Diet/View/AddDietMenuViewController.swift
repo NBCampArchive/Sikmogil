7f473c5cd545176658acb4d7b606adb1756c3010
Heather-Cho <gmlfkgmlfk95@gmail.com>
2024-06-07 19:55:05 +0900

    design #10 - diet view 생성

---
+++ Sikmogil/Presentation/Diet/View/AddDietMenuViewController.swift
@@ -0,0 +1,88 @@
+//
+//  AddDietMenuViewController.swift
+//  Sikmogil
+//
+//  Created by 희라 on 6/4/24.
+//  [View] **설명** 식사추가 페이지
+
+import UIKit
+import FloatingPanel
+
+class AddDietMenuViewController: UIViewController {
+    
+    let TitleLabel = UILabel().then {
+        $0.text = "식사 추가"
+        $0.textColor = .black
+        $0.font = Suite.bold.of(size: 28)
+        $0.textAlignment = .left
+    }
+    
+    let searchBar = UISearchBar().then {
+        $0.placeholder = "무슨 음식을 드셨나요?"
+        $0.searchTextField.backgroundColor = .white
+        $0.searchTextField.layer.borderWidth = 1
+        $0.searchTextField.layer.borderColor = UIColor.appBlack.cgColor
+        $0.searchTextField.layer.cornerRadius = 10
+    }
+    
+    let searchResultTableView = UITableView().then {
+        $0.backgroundColor = .systemGray6
+        $0.register(AddDietMenuTableViewCell.self, forCellReuseIdentifier: "AddDietMenuTableViewCell")
+        $0.separatorStyle = .none
+    }
+
+    override func viewDidLoad() {
+        view.backgroundColor = .white
+        setupViews()
+        setupConstraints()
+        
+        searchResultTableView.delegate = self
+        searchResultTableView.dataSource = self
+        
+    }
+    
+    
+    private func setupViews() {
+        view.addSubviews(TitleLabel, searchBar, searchResultTableView)
+    }
+    
+    private func setupConstraints() {
+        
+        TitleLabel.snp.makeConstraints{
+            $0.top.equalToSuperview().offset(100)
+            $0.leading.equalToSuperview().offset(16)
+        }
+        searchBar.snp.makeConstraints{
+            $0.top.equalTo(TitleLabel.snp.bottom).offset(16)
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().inset(16)
+        }
+        searchResultTableView.snp.makeConstraints{
+            $0.top.equalTo(searchBar.snp.bottom).offset(16)
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().inset(16)
+            $0.bottom.equalToSuperview().inset(16)
+        }
+    }
+
+}
+
+
+extension AddDietMenuViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "AddDietMenuTableViewCell", for: indexPath) as? AddDietMenuTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        // 셀 구성 (필요에 따라 설정)
+        //cell.textLabel?.text = "Item \(indexPath.row + 1)"
+        
+        return cell
+    }
+    
+}
---
+++ Sikmogil/Presentation/Diet/View/Cell/AddDietMenuTableViewCell.swift
@@ -0,0 +1,106 @@
+//
+//  AddDietTableViewCell.swift
+//  Sikmogil
+//
+//  Created by 희라 on 6/5/24.
+//  [Cell] **설명** 식사추가 페이지 테이블 뷰 셀
+
+import UIKit
+import SnapKit
+import Then
+
+class AddDietMenuTableViewCell: UITableViewCell {
+    
+    let loundView = UIView().then{
+        $0.backgroundColor = .appLightGray
+        $0.layer.cornerRadius = 15
+    }
+    
+    let contentArea = UIView().then{
+        $0.backgroundColor = .appLightGray
+    }
+    
+    let cellTitleLabel = UILabel().then {
+        $0.text = "흰 쌀밥"
+        $0.textColor = .appBlack
+        $0.font = Suite.semiBold.of(size: 18)
+        $0.textAlignment = .left
+    }
+    let cellInfoLabel = UILabel().then {
+        $0.text = "1공기 (210g)"
+        $0.textColor = .appDarkGray
+        $0.font = Suite.semiBold.of(size: 12)
+        $0.textAlignment = .left
+    }
+    let plusButton = UIButton().then {
+        $0.setImage(UIImage(named: "addRingDuotone"), for: .normal)
+    }
+    
+    let cellKcalLabel = UILabel().then {
+        $0.text = "300kcal"
+        $0.textColor = .appDarkGray
+        $0.font = Suite.semiBold.of(size: 12)
+        $0.textAlignment = .left
+    }
+    
+    
+    
+    static let identifier = "AddDietMenuTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupViews()
+        setupConstraints()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func setupViews() {
+        contentView.addSubview(loundView)
+        loundView.addSubview(contentArea)
+        contentArea.addSubviews(cellTitleLabel,cellInfoLabel,plusButton,cellKcalLabel)
+    }
+    
+    
+    private func setupConstraints() {
+        loundView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(4)
+            $0.bottom.equalToSuperview().inset(4)
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+        }
+        
+        contentArea.snp.makeConstraints{
+            $0.edges.equalToSuperview().inset(16)
+            $0.width.equalTo(332)
+            $0.height.equalTo(46)
+        }
+        
+        cellTitleLabel.snp.makeConstraints{
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview()
+        }
+        
+        cellInfoLabel.snp.makeConstraints{
+            $0.bottom.equalTo(contentArea.snp.bottom)
+            $0.leading.equalToSuperview()
+        }
+        
+        plusButton.snp.makeConstraints{
+            $0.top.equalToSuperview()
+            $0.trailing.equalTo(contentArea.snp.trailing)
+            $0.width.equalTo(24)
+            $0.height.equalTo(24)
+        }
+        
+        cellKcalLabel.snp.makeConstraints{
+            $0.bottom.equalTo(contentArea.snp.bottom)
+            $0.trailing.equalTo(contentArea.snp.trailing)
+        }
+    }
+}
