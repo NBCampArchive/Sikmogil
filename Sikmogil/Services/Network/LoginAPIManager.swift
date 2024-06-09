6c6991951d8e1dc2b3280ac99e8e12088fddb22b
HyunRyeol Park <devpark435@gmail.com>
2024-06-09 21:49:50 +0900

    feat #20 - Token Refresh func

--- Sikmogil/Services/Network/LoginAPIManager.swift
+++ Sikmogil/Services/Network/LoginAPIManager.swift
@@ -19,6 +19,7 @@ class LoginAPIManager{
     
     private init() {}
     
+    // MARK: - 로그인 시도 API
     func getAccessToken(authCode: String?, provider: String, completion: @escaping (Result<LoginModel, Error>) -> Void){
         let url = "\(baseURL)/api/\(provider)/token"
         // Body 설정
@@ -36,6 +37,30 @@ class LoginAPIManager{
                     completion(.success(tokenResponse))
                 case .failure(let error):
                     print("로그인 요청 에러")
+                    completion(.failure(error))
+                }
+            }
+    }
+    
+    // MARK: - 토큰 갱신 API
+    func refreshToken(completion: @escaping (Result<LoginModel, Error>) -> Void){
+        let url = "\(baseURL)/api/token/access"
+        let refreshToken = keychain.get("refreshToken")
+        let parameters: Parameters = [
+            "refreshToken" : refreshToken!,
+        ]
+        
+        AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default)
+            .validate()
+            .responseDecodable(of: LoginModel.self) { response in
+                switch response.result {
+                case .success(let tokenResponse):
+                    print("토큰 만료로 인한 토큰 갱신 성공")
+                    self.keychain.set(tokenResponse.data.accessToken, forKey: "accessToken")
+                    self.keychain.set(tokenResponse.data.refreshToken, forKey: "refreshToken")
+                    completion(.success(tokenResponse))
+                case .failure(let error):
+                    print("토큰 갱신 에러")
                     completion(.failure(error))
                 }
             }
