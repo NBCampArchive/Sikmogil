e612d380439acd78f9998e8457d316bf4197d742
HyunRyeol Park <devpark435@gmail.com>
2024-06-09 18:24:36 +0900

    feat #19 - add Exercsise API ManagerClass

---
+++ Sikmogil/Services/Network/ExerciseAPIManager.swift
@@ -0,0 +1,17 @@
+//
+//  ExerciseAPIManager.swift
+//  Sikmogil
+//
+//  Created by 박현렬 on 6/9/24.
+//
+
+import Foundation
+import Alamofire
+
+class ExerciseAPIManager {
+    
+    static let shared = ExerciseAPIManager()
+    
+    private init() {}
+    
+}
