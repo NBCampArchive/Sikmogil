2517de18c1e36c06136f6db2c13b649e7d927ec4
HyunRyeol Park <devpark435@gmail.com>
2024-06-09 19:23:08 +0900

    feat #19 - 운동 API 모델 파일 생성

---
+++ Sikmogil/Model/ExerciseModel.swift
@@ -0,0 +1,14 @@
+//
+//  ExerciseModel.swift
+//  Sikmogil
+//
+//  Created by 박현렬 on 6/9/24.
+//
+
+import Foundation
+
+struct ExerciseModel: Codable {
+    let workoutDate: String?
+    let steps: Int?
+    let totalCaloriesBurned: Int?
+}
