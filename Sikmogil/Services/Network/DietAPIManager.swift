7594711c6b0f1da72249ae4eb38d1d919b8bfaa9
HyunRyeol Park <devpark435@gmail.com>
2024-06-11 23:10:46 +0900

    feat #33 - Add DietAPIManager

---
+++ Sikmogil/Services/Network/DietAPIManager.swift
@@ -0,0 +1,17 @@
+//
+//  DietAPIManager.swift
+//  Sikmogil
+//
+//  Created by 박현렬 on 6/11/24.
+//
+
+import Foundation
+import Alamofire
+
+class DietAPIManager {
+    
+    static let shared = DietAPIManager()
+    
+    private init() {}
+    
+}
