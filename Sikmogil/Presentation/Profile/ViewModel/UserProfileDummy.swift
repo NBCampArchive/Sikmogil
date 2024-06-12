329ec9cf772faffb1bbb1733f0ee27ebecb0c2d5
Stay-jun <112465877+Neo-agnes@users.noreply.github.com>
2024-06-13 05:25:15 +0900

    feat/30 사용자 프로필 더미데이터

--- Sikmogil/Presentation/Profile/ViewModel/UserProfileDummy.swift
+++ Sikmogil/Presentation/Profile/ViewModel/UserProfileDummy.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+struct UserProfileDummy {
+    static let shared = UserProfile(
+        nickname: "Cats Green",
+        height: "170",
+        weight: "65",
+        gender: "남자",
+        targetWeight: "60",
+        toDate: "2024-06-12",
+        targetDate: "2024-12-12",
+        reminderTime: "09:00"
+    )
+}
