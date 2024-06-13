0ad4945e0e95602420d99c9afdcef7a1ed19e711
HyunRyeol Park <devpark435@gmail.com>
2024-06-13 20:31:01 +0900

    fix #42 - remove oldFloatingPanel

--- Sikmogil/Presentation/Main/View/CalendarViewController.swift
+++ Sikmogil/Presentation/Main/View/CalendarViewController.swift
@@ -14,7 +14,6 @@ import FloatingPanel
 
 class CalendarViewController: UIViewController {
     
-    var secondFloatingPanelController: FloatingPanelController!
     var editDiaryFloatingPanelController: FloatingPanelController!
     var secondDimmingView: UIView!
     
