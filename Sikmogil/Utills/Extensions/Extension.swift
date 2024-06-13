237839c644609a38f14a32118f69fef7d21b79fe
정유진 <yujin3504@gmail.com>
2024-06-13 20:29:01 +0900

    feat #32 - Add Double Extension

--- Sikmogil/Utills/Extensions/Extension.swift
+++ Sikmogil/Utills/Extensions/Extension.swift
@@ -21,3 +21,12 @@ extension UILabel {
     }
 }
 
+// MARK: - Double 값을 Int로 변환, 천 단위로 쉼표를 추가하여 문자열로 반환
+extension Double {
+    func formattedWithCommas() -> String {
+        let intValue = Int(self)
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        return numberFormatter.string(from: NSNumber(value: intValue)) ?? "\(intValue)"
+    }
+}
