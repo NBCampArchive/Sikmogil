d697cdcceccb5c86e8f8fcfab272902f316acc42
HyunRyeol Park <devpark435@gmail.com>
2024-06-15 21:27:17 +0900

    refactor #42 - with combine

---
+++ Sikmogil/Presentation/Main/ViewModel/CalendarViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CalendarViewModel.swift
+//  Sikmogil
+//
+//  Created by 박현렬 on 6/15/24.
+//
+
+import Foundation
--- Sikmogil/Services/Network/CalendarAPIManager.swift
+++ Sikmogil/Services/Network/CalendarAPIManager.swift
@@ -27,7 +27,7 @@ class CalendarAPIManager {
     }
     
     // MARK: - 캘린더 데이터 업데이트
-    func updateCalendarData(calendarDate: String, diaryText: String, completion: @escaping (Result<Void, Error>) -> Void) {
+    func updateCalendarData(calendarDate: String, diaryText: String) -> AnyPublisher<Void, Error> {
         let url = "\(baseURL)/api/calendar/UpdateCalendar"
         
         let parameters: [String: Any] = [
@@ -35,34 +35,31 @@ class CalendarAPIManager {
             "diaryText": diaryText,
         ]
         
-        AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers).response { response in
-            switch response.result {
-            case .success:
-                print("updateCalendarData success")
-                completion(.success(()))
-            case .failure(let error):
-                completion(.failure(error))
+        return AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: headers)
+            .validate()
+            .publishData()
+            .tryMap { response in
+                if let error = response.error {
+                    throw error
+                }
+                return ()
             }
-        }
+            .eraseToAnyPublisher()
     }
     
     // MARK: - 캘린더 전체 데이터 출력
-    func getAllCalendarData(completion: @escaping (Result<[CalendarModel], Error>) -> Void) {
+    func getAllCalendarData() -> AnyPublisher<[CalendarModel], Error> {
         let url = "\(baseURL)/api/calendar"
         
-        AF.request(url, method: .get, encoding: JSONEncoding.default, headers: headers).responseDecodable(of: [CalendarModel].self) { response in
-            switch response.result {
-            case .success(let data):
-                print("getAllCalendarData success")
-                completion(.success(data))
-            case .failure(let error):
-                completion(.failure(error))
-            }
-        }
+        return AF.request(url, method: .get, encoding: JSONEncoding.default, headers: headers)
+            .publishDecodable(type: [CalendarModel].self)
+            .value()
+            .mapError { $0 as Error }
+            .eraseToAnyPublisher()
     }
     
     // MARK: - 특정 날짜 캘린더 데이터 출력
-    func getCalendarData(calendarDate: String, completion: @escaping (Result<[CalendarModel], Error>) -> Void) {
+    func getCalendarData(calendarDate: String) -> AnyPublisher<[CalendarModel], Error>{
         
         let url = "\(baseURL)/api/calendar"
         
@@ -70,15 +67,11 @@ class CalendarAPIManager {
             "diaryDate": calendarDate
         ]
         
-        AF.request(url, method: .get, parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseDecodable(of: [CalendarModel].self) { response in
-            switch response.result {
-            case .success(let data):
-                print("getCalendarData success")
-                completion(.success(data))
-            case .failure(let error):
-                completion(.failure(error))
-            }
-        }
+        return AF.request(url, method: .get, parameters: parameters, encoding: JSONEncoding.default, headers: headers)
+            .publishDecodable(type: [CalendarModel].self)
+            .value()
+            .mapError { $0 as Error}
+            .eraseToAnyPublisher()
     }
     
     //MARK: - 특정 날짜 몸무게 업데이트
@@ -114,30 +107,3 @@ class CalendarAPIManager {
     }
     
 }
-// MARK: - Example Code
-//CalendarAPIManager.shared.updateCalendarData(calendarDate: DateHelper.shared.formatDateToYearMonthDay(Date()), diaryText: "다이어리 테스트 1"){ result in
-//    switch result {
-//    case .success:
-//        print("다이어리 업데이트 성공")
-//    case .failure(let error):
-//        print("다이어리 업데이트 실패: \(error)")
-//    }
-//}
-//
-//CalendarAPIManager.shared.getCalendarData(calendarDate: DateHelper.shared.formatDateToYearMonthDay(Date())){ result in
-//    switch result {
-//    case .success(let data):
-//        print("다이어리 데이터 가져오기 성공: \(data)")
-//    case .failure(let error):
-//        print("다이어리 데이터 가져오기 실패: \(error)")
-//    }
-//}
-//
-//CalendarAPIManager.shared.getAllCalendarData() { result in
-//    switch result {
-//    case .success(let data):
-//        print("다이어리 전체 데이터 가져오기 성공: \(data)")
-//    case .failure(let error):
-//        print("다이어리 전체 데이터 가져오기 실패: \(error)")
-//    }
-//}
