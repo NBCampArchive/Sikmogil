752b3284a8349ee1834cb2a6d199c1bcdc70e2de
Stay-jun <112465877+Neo-agnes@users.noreply.github.com>
2024-06-18 19:17:42 +0900

    feat/#64 게시글, 공감글 뷰 컨트롤러 생성

--- Sikmogil/Presentation/Profile/View/LikedPostViewController.swift
+++ Sikmogil/Presentation/Profile/View/LikedPostViewController.swift
@@ -3,6 +3,11 @@
 //  Sikmogil
 //
 //  Created by Developer_P on 6/18/24.
-//
+//  [공감글] ♥️ 공감글 ♥️
 
+import UIKit
 import Foundation
+
+class LikedPostViewController: UIViewController {
+    
+}
--- Sikmogil/Presentation/Profile/View/PostViewController.swift
+++ Sikmogil/Presentation/Profile/View/PostViewController.swift
@@ -3,6 +3,11 @@
 //  Sikmogil
 //
 //  Created by Developer_P on 6/18/24.
-//
+//  [게시글] ✍🏻 게시글 ✍🏻
 
+import UIKit
 import Foundation
+
+class PostViewController: UIViewController {
+    
+}
