a60d45951a979c5df4efc304ba0946faae38c669
Stay-jun <112465877+Neo-agnes@users.noreply.github.com>
2024-06-20 21:34:09 +0900

    feat/#64 작성자 수정

--- Sikmogil/Utills/Extensions/UITextField.swift
+++ Sikmogil/Utills/Extensions/UITextField.swift
@@ -2,13 +2,13 @@
 //  UITextField.swift
 //  Sikmogil
 //
-//  Created by Developer_P on 6/19/24.
-//
+//  Created by 박준영 on 6/19/24.
+//  [extension] **설명** UI텍스트 필드의 옵셔널 처리를 간단하게 하기 위해서
 
 import Foundation
 import Combine
 import UIKit
-// MARK: - 옵셔널 처리를 간단하게 하기 위해서
+
 extension UITextField {
     var textPublisher: AnyPublisher<String?, Never> {
         NotificationCenter.default.publisher(for: UITextField.textDidChangeNotification, object: self)
