8b80fd7d2dae2016e99b22d0348d979250a9aee5
Heather-Cho <gmlfkgmlfk95@gmail.com>
2024-06-20 14:31:12 +0900

    feat #52 - 테이블뷰 셀 추가

--- Sikmogil/Presentation/Diet/View/Cell/DinnerTableViewCell.swift
+++ Sikmogil/Presentation/Diet/View/Cell/DinnerTableViewCell.swift
@@ -2,22 +2,58 @@
 //  DinnerTableViewCell.swift
 //  Sikmogil
 //
-//  Created by t2023-m0114 on 6/19/24.
+//  Created by 희라 on 6/19/24.
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class DinnerTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    static let identifier = "DinnerTableViewCell"
+    
+    // MARK: - UI components
+    let nameLabel = UILabel().then {
+        $0.text = "식품명"
+        $0.textColor = .appDarkGray
+        $0.font = Suite.semiBold.of(size: 14)
+        $0.textAlignment = .left
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    let kcalLabel = UILabel().then {
+        $0.text = "칼로리"
+        $0.textColor = .appDarkGray
+        $0.font = Suite.semiBold.of(size: 14)
+        $0.textAlignment = .left
     }
-
+    
+    // MARK: - View Lifecycle
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupViews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup Methods
+    private func setupViews() {
+        contentView.addSubviews(nameLabel,kcalLabel)
+    }
+    
+    private func setupConstraints() {
+        nameLabel.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.bottom.equalToSuperview()
+            $0.leading.equalToSuperview().offset(32)
+            $0.trailing.equalTo(kcalLabel.snp.leading).offset(8)
+        }
+        kcalLabel.snp.makeConstraints{
+            $0.top.equalToSuperview()
+            $0.bottom.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(20)
+        }
+    }
 }
