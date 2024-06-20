1305c6bbaf9a58a9951c16c4d9156155edc39452
Heather-Cho <gmlfkgmlfk95@gmail.com>
2024-06-20 14:30:27 +0900

    fix #52 - 플로팅패널 크기조정 수정

--- Sikmogil/Presentation/Diet/View/WaterBottomSheetViewController.swift
+++ Sikmogil/Presentation/Diet/View/WaterBottomSheetViewController.swift
@@ -104,19 +104,14 @@ class WaterBottomSheetViewController: UIViewController {
         if let userInfo = notification.userInfo,
            let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect,
            let fpc = parent as? FloatingPanelController {
-            let keyboardHeight = keyboardFrame.height
-            let initialHeight = self.view.bounds.height - 460
-            fpc.surfaceLocation = CGPoint(x: self.view.bounds.midX, y: initialHeight)
-            fpc.surfaceView.containerMargins.bottom = keyboardHeight
+            fpc.move(to: .full, animated: true)
         }
     }
     
     @objc override func keyboardWillHide(notification: NSNotification) {
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
         if let fpc = parent as? FloatingPanelController {
-            let initialHeight = self.view.bounds.height + 100
-            fpc.surfaceLocation = CGPoint(x: self.view.bounds.midX, y: initialHeight)
-            fpc.surfaceView.containerMargins.bottom = 0
+            fpc.move(to: .half, animated: true)
         }
     }
     
@@ -132,14 +127,7 @@ class WaterBottomSheetViewController: UIViewController {
         // 싱글톤 인스턴스를 통해 데이터 업데이트
         WaterViewModel.shared.addWaterAmount(waterAmount)
         
-        if let fpc = parent as? FloatingPanelController {
-            fpc.move(to: .hidden, animated: true)
-            fpc.hide(animated: true){
-                self.tabBarController?.tabBar.isHidden = false
-                fpc.view.removeFromSuperview()
-                fpc.removeFromParent()
-            }
-        }
+        self.dismiss(animated: true)
     }
 }
 
