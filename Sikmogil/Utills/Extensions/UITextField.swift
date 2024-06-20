cc3e3da5bc541cc61cfec4fe6c204e6e57fbc15a
Stay-jun <112465877+Neo-agnes@users.noreply.github.com>
2024-06-20 16:07:04 +0900

    feat/#64 옵셔널 처리 부분추가

--- Sikmogil/Utills/Extensions/UITextField.swift
+++ Sikmogil/Utills/Extensions/UITextField.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import Combine
+import UIKit
+// MARK: - 옵셔널 처리를 간단하게 하기 위해서
+extension UITextField {
+    var textPublisher: AnyPublisher<String?, Never> {
+        NotificationCenter.default.publisher(for: UITextField.textDidChangeNotification, object: self)
+            .map { ($0.object as? UITextField)?.text }
+            .eraseToAnyPublisher()
+    }
+}
