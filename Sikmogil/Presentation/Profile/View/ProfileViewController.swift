9a365a8e179db6eb66cf0642c7032d6592450144
HyunRyeol Park <devpark435@gmail.com>
2024-06-23 06:35:10 +0900

    refactor #86 - 1차 배포 내용 외 숨김 처리

--- Sikmogil/Presentation/Profile/View/ProfileViewController.swift
+++ Sikmogil/Presentation/Profile/View/ProfileViewController.swift
@@ -39,22 +39,22 @@ class ProfileViewController: UIViewController {
         $0.clipsToBounds = true
     }
     
-    let levelBadgeView = UIImageView().then {
-        $0.image = .levelbar
-    }
+//    let levelBadgeView = UIImageView().then {
+//        $0.image = .levelbar
+//    }
     
-    let levelBadgeLabel = UILabel().then {
-        $0.text = "Lv.0"
-        $0.font = Suite.regular.of(size: 10)
-        $0.textColor = .appBlack
-        $0.textAlignment = .right
-    }
+//    let levelBadgeLabel = UILabel().then {
+//        $0.text = "Lv.0"
+//        $0.font = Suite.regular.of(size: 10)
+//        $0.textColor = .appBlack
+//        $0.textAlignment = .right
+//    }
     
-    let levelStackView = UIStackView().then {
-        $0.axis = .horizontal
-        $0.alignment = .center
-        $0.spacing = 4
-    }
+//    let levelStackView = UIStackView().then {
+//        $0.axis = .horizontal
+//        $0.alignment = .center
+//        $0.spacing = 4
+//    }
     
     var nicknameLabel = UILabel().then {
         $0.text = "아무개"
@@ -62,12 +62,12 @@ class ProfileViewController: UIViewController {
     }
     
     let profileInfoView = ProfileInfoView()
-    let profileTableView = ProfileTableView()
+//    let profileTableView = ProfileTableView()
     
     let logoutButton = UIButton().then {
         $0.setTitle("로그아웃", for: .normal)
-        $0.setTitleColor(.appDarkGray, for: .normal)
-        $0.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+        $0.setTitleColor(.appBlack, for: .normal)
+        $0.titleLabel?.font = Suite.bold.of(size: 16)
     }
     
     // MARK: - viewDidLoad
@@ -92,6 +92,8 @@ class ProfileViewController: UIViewController {
         super.viewWillAppear(animated)
         viewModel.fetchUserProfile()
         setupBindings()
+        setTabBar(hidden: false, animated: true)
+        navigationController?.navigationBar.isHidden = true
     }
 
     // MARK: - Binding
@@ -135,13 +137,13 @@ class ProfileViewController: UIViewController {
         topBar.addSubview(profileLabel)
         topBar.addSubview(settingsButton)
         
-        [profileImageView, levelBadgeView, nicknameLabel, profileInfoView, profileTableView, logoutButton].forEach {
+        [profileImageView, /*levelBadgeView,*/ nicknameLabel, profileInfoView,/* profileTableView,*/ logoutButton].forEach {
             contentView.addSubview($0)
         }
         
-        levelStackView.addArrangedSubview(levelBadgeLabel)
-        levelBadgeView.addSubview(levelStackView)
-        levelStackView.addArrangedSubview(spacerView)
+//        levelStackView.addArrangedSubview(levelBadgeLabel)
+//        levelBadgeView.addSubview(levelStackView)
+//        levelStackView.addArrangedSubview(spacerView)
         
         setupConstraints()
     }
@@ -153,12 +155,12 @@ class ProfileViewController: UIViewController {
         }
         
         contentView.snp.makeConstraints {
-            $0.edges.equalTo(scrollView)
-            $0.width.equalTo(scrollView)
+            $0.edges.equalToSuperview()
+            $0.width.equalToSuperview()
         }
         
         topBar.snp.makeConstraints {
-            $0.top.equalTo(contentView).offset(0)
+            $0.top.equalTo(contentView).offset(32)
             $0.left.right.equalTo(contentView)
             $0.height.equalTo(60)
         }
@@ -180,27 +182,12 @@ class ProfileViewController: UIViewController {
             $0.width.height.equalTo(100)
         }
         
-        levelBadgeView.snp.makeConstraints {
-            $0.top.equalTo(profileImageView.snp.bottom).offset(-20)
-            $0.centerX.equalTo(contentView)
-            $0.height.equalTo(24)
-            $0.width.equalTo(60)
-        }
-        
-        levelStackView.snp.makeConstraints {
-            $0.edges.equalToSuperview().inset(4)
-        }
-        
-        levelBadgeLabel.snp.makeConstraints {
-            $0.height.equalToSuperview()
-        }
-        
         spacerView.snp.makeConstraints {
             $0.width.equalTo(4)
         }
         
         nicknameLabel.snp.makeConstraints {
-            $0.top.equalTo(levelBadgeView.snp.bottom).offset(10)
+            $0.top.equalTo(profileImageView.snp.bottom).offset(10)
             $0.centerX.equalTo(contentView)
         }
         
@@ -211,16 +198,17 @@ class ProfileViewController: UIViewController {
             $0.height.equalTo(100)
         }
         
-        profileTableView.snp.makeConstraints {
-            $0.top.equalTo(profileInfoView.snp.bottom).offset(32)
-            $0.left.equalTo(contentView).offset(16)
-            $0.right.equalTo(contentView).offset(-16)
-            $0.height.equalTo(200)
-        }
+//        profileTableView.snp.makeConstraints {
+//            $0.top.equalTo(profileInfoView.snp.bottom).offset(32)
+//            $0.left.equalTo(contentView).offset(16)
+//            $0.right.equalTo(contentView).offset(-16)
+//            $0.height.equalTo(200)
+//        }
         
         logoutButton.snp.makeConstraints {
             $0.centerX.equalTo(contentView)
-            $0.top.equalTo(profileTableView.snp.bottom).offset(240)
+//            $0.top.equalTo(profileTableView.snp.bottom).offset(240)
+            $0.top.equalTo(profileInfoView.snp.bottom).offset(32)
             $0.bottom.equalTo(contentView).offset(-20)
         }
     }
