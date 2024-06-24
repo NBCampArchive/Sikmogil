92953d96b5f2e9cb2bb24af6ebb755507e27752d
HyunRyeol Park <devpark435@gmail.com>
2024-06-24 18:52:58 +0900

    refactor #86 - 시작화면 splash로 고정

--- Sikmogil/Application/SceneDelegate.swift
+++ Sikmogil/Application/SceneDelegate.swift
@@ -20,51 +20,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         
-        // MARK: - API Test 시 아래 코드를 주석 처리하고 진행해주세요 (간단한 UI 수정시에만 사용해주세요)
-//        let viewController = LoginViewController()
-//        let navigationController = UINavigationController(rootViewController: viewController)
-//        
-//        window.rootViewController = navigationController
-//        self.window = window
-//        window.makeKeyAndVisible()
+        // MARK: - viewController = 부분을 수정하고 아래 코드를 주석 처리하고 진행해주세요 (간단한 UI 수정시에만 사용해주세요, 기본은 SplashViewController)
+        let viewController = SplashViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
         
-        // MARK: - API Test 시 아래 코드를 활성화 하고 진행해주세요
-        // 토큰 유효성 검사
-        if KeychainSwift().get("refreshToken") != nil {
-            // Refresh 토큰이 있으면 토큰 갱신 시도
-            LoginAPIManager.shared.refreshToken { result in
-                switch result {
-                case .success:
-                    // 토큰 갱신 성공, 메인 화면으로 이동
-                    DispatchQueue.main.async {
-                        print("메인화면 이동")
-                        self.showMainScreen()
-                    }
-                case .failure:
-                    // 토큰 갱신 실패, 로그인 화면으로 이동
-                    DispatchQueue.main.async {
-                        print("로그인 이동")
-                        self.showLoginScreen()
-                    }
-                }
-            }
-        } else {
-            // Refresh 토큰이 없으면 로그인 화면으로 이동
-            showLoginScreen()
-        }
-        
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
-        //여기까지 주석 처리
-    }
-    
-    private func showLoginScreen() {
-        let loginViewController = LoginViewController()
-        window?.rootViewController = loginViewController
-    }
-    
-    private func showMainScreen() {
-        let mainViewController = BottomTabBarController()
-        window?.rootViewController = mainViewController
     }
     
     func sceneDidDisconnect(_ scene: UIScene) {
