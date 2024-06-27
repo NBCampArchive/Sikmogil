70d9a0fdb839a23efe6d16ce49ae85e9fd1991f4
HyunRyeol Park <devpark435@gmail.com>
2024-06-27 15:46:22 +0900

    feat #99 - 토큰 저장소 분리

--- Sikmogil/Services/PersistentStorage/TokenStorage.swift
+++ Sikmogil/Services/PersistentStorage/TokenStorage.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import KeychainSwift
+
+class TokenStorage {
+    static let shared = TokenStorage()
+    private let keychain = KeychainSwift()
+    
+    var accessToken: String? {
+        return keychain.get("accessToken")
+    }
+    
+    var refreshToken: String? {
+        return keychain.get("refreshToken")
+    }
+    
+    func setTokens(accessToken: String, refreshToken: String) {
+        keychain.set(accessToken, forKey: "accessToken")
+        keychain.set(refreshToken, forKey: "refreshToken")
+    }
+    
+    func clearTokens() {
+        keychain.delete("accessToken")
+        keychain.delete("refreshToken")
+    }
+}
+
