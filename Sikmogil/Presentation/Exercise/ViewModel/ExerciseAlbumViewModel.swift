8c0ed066b5a6fb3fe4adf20de25f19a5d28c342f
정유진 <yujin3504@gmail.com>
2024-06-28 20:10:45 +0900

    feat #71 - getExercisePictures

--- Sikmogil/Presentation/Exercise/ViewModel/ExerciseAlbumViewModel.swift
+++ Sikmogil/Presentation/Exercise/ViewModel/ExerciseAlbumViewModel.swift
@@ -10,20 +10,37 @@ import Combine
 
 class ExerciseAlbumViewModel: ObservableObject {
     
-    @Published var exercisePictures: [ExerciseListModel] = []
+    @Published var exerciseAlbum: ExerciseAlbum?
+    
+    private var currentPage: Int = 0
+    private var isFetching = false
+    
+    var exercisePictures: [ExerciseListModel] {
+        return exerciseAlbum?.pictures ?? []
+    }
+    
     var cancellables = Set<AnyCancellable>()
     
     init() {
-        getExercisePictures()
+        // 초기 데이터 가져오기 (필요한 경우)
+        getExercisePictures(page: 0)
     }
     
-    func getExercisePictures() {
-        ExerciseAPIManager.shared.getExercisePicture { [weak self] result in
+    func getExercisePictures(page: Int) {
+        ExerciseAPIManager.shared.getExercisePicture(page: page) { [weak self] result in
+            guard let self = self else { return }
+            self.isFetching = false
+            
             switch result {
             case .success(let data):
                 print("이미지 데이터", data)
                 DispatchQueue.main.async {
-                    self?.exercisePictures = data
+                    if page == 0 {
+                        self.exerciseAlbum = data
+                    } else {
+                        self.exerciseAlbum?.pictures.append(contentsOf: data.pictures)
+                    }
+                    self.currentPage = page
                 }
             case .failure(let error):
                 print("Failed to fetch exercise pictures: \(error)")
