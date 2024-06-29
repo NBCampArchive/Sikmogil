488b68ce05fb21164af339136d21bdb36ed74317
정유진 <yujin3504@gmail.com>
2024-06-30 01:22:53 +0900

    feat #71 - PhotoSelectVC 구현

--- Sikmogil/Presentation/Exercise/View/PhotoSelectViewController.swift
+++ Sikmogil/Presentation/Exercise/View/PhotoSelectViewController.swift
@@ -6,24 +6,43 @@
 //
 
 import UIKit
+import SnapKit
+import Then
+import Kingfisher
 
 class PhotoSelectViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    private var imageURL: URL
+    
+    
+    private var imageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    init(imageURL: URL) {
+        self.imageURL = imageURL
+        super.init(nibName: nil, bundle: nil)
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+    
+    private func setupViews() {
+        view.backgroundColor = .white
+        view.addSubview(imageView)
+        
+        imageView.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(16)
+        }
 
+        imageView.kf.setImage(with: imageURL, placeholder: nil, options: [
+            .transition(.fade(0.5))
+        ])
+    }
 }
