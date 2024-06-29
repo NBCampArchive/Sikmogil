dd662223d4e003899096fac0fef685f497ccae5a
정유진 <yujin3504@gmail.com>
2024-06-30 01:45:39 +0900

    feat #71 - 이미지뷰 레이아웃 설정

--- Sikmogil/Presentation/Exercise/View/PhotoSelectViewController.swift
+++ Sikmogil/Presentation/Exercise/View/PhotoSelectViewController.swift
@@ -11,13 +11,8 @@ import Then
 import Kingfisher
 
 class PhotoSelectViewController: UIViewController {
-
+    private var imageView = UIImageView()
     private var imageURL: URL
-    
-    
-    private var imageView = UIImageView().then {
-        $0.contentMode = .scaleAspectFit
-    }
     
     init(imageURL: URL) {
         self.imageURL = imageURL
@@ -30,19 +25,50 @@ class PhotoSelectViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupViews()
+        view.backgroundColor = .black
+        setupView()
     }
     
-    private func setupViews() {
-        view.backgroundColor = .white
-        view.addSubview(imageView)
+    private func setupView() {
+        imageView.contentMode = .scaleAspectFit
         
-        imageView.snp.makeConstraints {
-            $0.edges.equalToSuperview().inset(16)
-        }
-
+        view.addSubview(imageView)
+        imageView.snp.makeConstraints { $0.edges.equalToSuperview() }
+        
+        loadImageAndAdjustLayout()
+    }
+    
+    private func loadImageAndAdjustLayout() {
         imageView.kf.setImage(with: imageURL, placeholder: nil, options: [
             .transition(.fade(0.5))
-        ])
+        ]) { result in
+            switch result {
+            case .success(let value):
+                self.adjustImageViewLayout(for: value.image)
+            case .failure(let error):
+                print("이미지 로드 실패: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    private func adjustImageViewLayout(for image: UIImage) {
+        let imageViewAspectRatio = image.size.width / image.size.height
+        let screenAspectRatio = view.frame.width / view.frame.height
+        
+        if imageViewAspectRatio > screenAspectRatio {
+            // 가로가 더 긴 경우
+            imageView.snp.remakeConstraints {
+                $0.leading.trailing.equalToSuperview()
+                $0.height.equalTo(imageView.snp.width).multipliedBy(1.0 / imageViewAspectRatio)
+                $0.centerY.equalToSuperview()
+            }
+        } else {
+            // 세로가 더 긴 경우
+            imageView.snp.remakeConstraints {
+                $0.top.bottom.equalToSuperview()
+                $0.width.equalTo(imageView.snp.height).multipliedBy(imageViewAspectRatio)
+                $0.centerX.equalToSuperview()
+            }
+        }
     }
 }
