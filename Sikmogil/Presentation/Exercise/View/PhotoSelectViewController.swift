0735cdbb83b37ec11d1d0ecd6ed14437c2afa179
정유진 <yujin3504@gmail.com>
2024-07-01 22:47:59 +0900

    design #111 - PhotoSelectVC UI 구성

--- Sikmogil/Presentation/Exercise/View/PhotoSelectViewController.swift
+++ Sikmogil/Presentation/Exercise/View/PhotoSelectViewController.swift
@@ -11,11 +11,24 @@ import Then
 import Kingfisher
 
 class PhotoSelectViewController: UIViewController {
-    private var imageView = UIImageView()
-    private var imageURL: URL
     
-    init(imageURL: URL) {
+    private var imageURL: URL
+    private var titleText: String
+    
+    private var imageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    private var deleteButton = UIButton().then {
+        let trashImage = UIImage(systemName: "trash")?.withTintColor(UIColor.white, renderingMode: .alwaysOriginal)
+        $0.setImage(trashImage, for: .normal)
+        $0.setTitle("  삭제", for: .normal)
+        $0.titleLabel?.font = Suite.semiBold.of(size: 16)
+    }
+    
+    init(imageURL: URL, title: String) {
         self.imageURL = imageURL
+        self.titleText = title
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -26,49 +39,64 @@ class PhotoSelectViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
-        setupView()
+        navigationItem.title = titleText
+        
+        setupViews()
+        setupConstraints()
     }
     
-    private func setupView() {
-        imageView.contentMode = .scaleAspectFit
-        
-        view.addSubview(imageView)
-        imageView.snp.makeConstraints { $0.edges.equalToSuperview() }
-        
-        loadImageAndAdjustLayout()
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // 네비게이션 아이템 색상 변경
+        navigationController?.navigationBar.tintColor = .white
+        let textAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        navigationController?.navigationBar.scrollEdgeAppearance?.titleTextAttributes = textAttributes
     }
     
-    private func loadImageAndAdjustLayout() {
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // 네비게이션 아이템 색상 복원
+        navigationController?.navigationBar.tintColor = .appBlack
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.appBlack]
+    }
+    
+    private func setupViews() {
+        view.addSubviews(imageView, deleteButton)
+        
+        loadImage()
+        deleteButton.addTarget(self, action: #selector(deleteButtonTapped), for: .touchUpInside)
+    }
+    
+    private func loadImage() {
         imageView.kf.setImage(with: imageURL, placeholder: nil, options: [
             .transition(.fade(0.5))
         ]) { result in
             switch result {
-            case .success(let value):
-                self.adjustImageViewLayout(for: value.image)
+            case .success(_):
+                print("이미지 로드 성공")
             case .failure(let error):
                 print("이미지 로드 실패: \(error.localizedDescription)")
             }
         }
     }
     
-    private func adjustImageViewLayout(for image: UIImage) {
-        let imageViewAspectRatio = image.size.width / image.size.height
-        let screenAspectRatio = view.frame.width / view.frame.height
-        
-        if imageViewAspectRatio > screenAspectRatio {
-            // 가로가 더 긴 경우
-            imageView.snp.remakeConstraints {
-                $0.leading.trailing.equalToSuperview()
-                $0.height.equalTo(imageView.snp.width).multipliedBy(1.0 / imageViewAspectRatio)
-                $0.centerY.equalToSuperview()
-            }
-        } else {
-            // 세로가 더 긴 경우
-            imageView.snp.remakeConstraints {
-                $0.top.bottom.equalToSuperview()
-                $0.width.equalTo(imageView.snp.height).multipliedBy(imageViewAspectRatio)
-                $0.centerX.equalToSuperview()
-            }
+    private func setupConstraints() {
+        imageView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-60)
+            $0.leading.trailing.equalToSuperview()
         }
+        
+        deleteButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide)
+            $0.width.equalTo(80)
+            $0.height.equalTo(40)
+        }
+    }
+    
+    @objc private func deleteButtonTapped() {
+        // TODO: - 이미지 삭제 로직 구현
+        print("삭제 버튼")
     }
 }
