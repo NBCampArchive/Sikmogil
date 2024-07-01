6753e8594c9041330cf42091c3ef1d71cb2e865f
HyunRyeol Park <devpark435@gmail.com>
2024-07-01 18:56:48 +0900

    add #116 - BoardAPI Manager feature of fetch list

--- Sikmogil/Services/Network/BoardAPIManager.swift
+++ Sikmogil/Services/Network/BoardAPIManager.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import Alamofire
+import Combine
+
+class BoardAPIManager {
+    
+    static let shared = BoardAPIManager()
+    
+    private init() {}
+    
+    private let baseURL = Bundle.main.baseURL
+    
+    private let session: Session = {
+        let interceptor = AuthInterceptor()
+        return Session(interceptor: interceptor)
+    }()
+    
+    func fetchBoardList() -> AnyPublisher<BoardListResponse, Error> {
+        let url = "\(baseURL)/api/board/list"
+        
+        return session.request(url, method: .get)
+            .publishDecodable(type: BoardListResponse.self)
+            .value()
+            .mapError { $0 as Error }
+            .eraseToAnyPublisher()
+    }
+}
