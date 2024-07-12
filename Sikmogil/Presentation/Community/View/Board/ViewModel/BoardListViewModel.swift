8402122c255de3d86fd6897d7f582d8bdc0a2164
devpark435 <devpark435@gmail.com>
2024-07-12 16:27:51 +0900

    refactor #122 - 페이지네이션 수정

--- Sikmogil/Presentation/Community/View/Board/ViewModel/BoardListViewModel.swift
+++ Sikmogil/Presentation/Community/View/Board/ViewModel/BoardListViewModel.swift
@@ -13,6 +13,8 @@ class BoardListViewModel {
     @Published var errorMessage: String?
     @Published var isLoading: Bool = false
     @Published var hasMoreData: Bool = true
+    @Published var isRefreshing: Bool = false
+    @Published var isPaging: Bool = false
     
     private var cancellables = Set<AnyCancellable>()
     private var currentPage: Int = 0
@@ -31,19 +33,23 @@ class BoardListViewModel {
         BoardAPIManager.shared.fetchBoardList(category: category, page: page)
             .print("fetchBoardList")
             .receive(on: DispatchQueue.main)
-            .sink(receiveCompletion: { completion in
+            .sink(receiveCompletion: { [weak self] completion in
+                guard let self = self else { return }
                 self.isLoading = false
+                self.isRefreshing = false
+                self.isPaging = false
                 switch completion {
                 case .finished:
                     break
                 case .failure(let error):
                     self.errorMessage = error.localizedDescription
                 }
-            }, receiveValue: { response in
+            }, receiveValue: { [weak self] response in
+                guard let self = self else { return }
                 self.boardList.append(contentsOf: response.data.boardInfoResDtos.content)
-                self.currentPage = response.data.boardInfoResDtos.page.number
+                self.currentPage = response.data.boardInfoResDtos.page.number + 1 // 다음 페이지로 이동
                 self.totalPages = response.data.boardInfoResDtos.page.totalPages
-                self.hasMoreData = self.currentPage < self.totalPages - 1
+                self.hasMoreData = self.currentPage < self.totalPages
             })
             .store(in: &cancellables)
     }
