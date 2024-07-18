e2875fb4bf70f6e81d72975cc945ae62bb151cf2
devpark435 <devpark435@gmail.com>
2024-07-18 22:53:01 +0900

    add #141 - 게시판 디테일 뷰컨트롤러, 뷰모델 파일 추가

---
+++ Sikmogil/Presentation/Community/View/Board/View/BoardDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BoardDetailViewController.swift
+//  Sikmogil
+//
+//  Created by 박현렬 on 7/18/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+import Combine
+import Kingfisher
+import SkeletonView
+
+class BoardDetailViewController: UIViewController {
+    
+}
---
+++ Sikmogil/Presentation/Community/View/Board/ViewModel/BoardDetailViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  BoardDetailViewModel.swift
+//  Sikmogil
+//
+//  Created by 박현렬 on 7/18/24.
+//
+
+import Foundation
+import Combine
+
+class BoardDetailViewModel {
+    
+}
