eea4908558465e2d4c5795a8ea56205c48e43ab8
devpark435 <devpark435@gmail.com>
2024-07-21 22:33:39 +0900

    add #141 - add CommentList Response Model

--- Sikmogil/Model/CommentListModel.swift
+++ Sikmogil/Model/CommentListModel.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+struct CommentResponse: Codable {
+    let statusCode: Int
+    let message: String
+    let data: CommentData
+}
+
+struct CommentData: Codable {
+    let commentInfoResDtos: CommentPage
+}
+
+struct CommentPage: Codable {
+    let content: [CommentInfo]
+    let page: PageInfo
+}
+
+struct CommentInfo: Codable {
+    let commentId: Int
+    let content: String
+    let writerNickname: String
+    let writerProfileImage: String?
+    let createdAt: String
+}
+
+struct PageInfo: Codable {
+    let size: Int
+    let number: Int
+    let totalElements: Int
+    let totalPages: Int
+}
