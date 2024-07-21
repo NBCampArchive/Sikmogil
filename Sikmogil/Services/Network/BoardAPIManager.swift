27ee34783c949560bd776d30d16ab48703363552
devpark435 <devpark435@gmail.com>
2024-07-22 03:09:08 +0900

    feat #141 - Update API Function

--- Sikmogil/Services/Network/BoardAPIManager.swift
+++ Sikmogil/Services/Network/BoardAPIManager.swift
@@ -54,4 +54,106 @@ class BoardAPIManager {
             .mapError { $0 as Error }
             .eraseToAnyPublisher()
     }
+    
+    func fetchBoardDetail(boardId: Int) -> AnyPublisher<BoardDetailModel, Error> {
+        let url = "\(baseURL)/api/boards/detail/\(boardId)"
+        
+        let parameters: [String: Any] = [
+            "boardId": boardId
+        ]
+        
+        return session.request(url, method: .get, parameters: parameters)
+            .publishDecodable(type: BoardDetailModel.self)
+            .value()
+            .mapError { $0 as Error }
+            .eraseToAnyPublisher()
+    }
+    
+    func toggleLike(boardId: Int) -> AnyPublisher<Void, Error> {
+        let url = "\(baseURL)/api/boards/likes/like"
+        
+        let parameters: [String: Any] = [
+            "boardId": boardId
+        ]
+        
+        return session.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default)
+            .validate()
+            .publishData()
+            .tryMap{ response in
+                if response.error != nil {
+                    throw response.error!
+                }
+                return ()
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    func deleteLike(boardId: Int) -> AnyPublisher<Void, Error> {
+        let url = "\(baseURL)/api/boards/likes/cancel"
+        
+        let parameters: [String: Any] = [
+            "boardId": boardId
+        ]
+        
+        return session.request(url, method: .delete, parameters: parameters, encoding: JSONEncoding.default)
+            .validate()
+            .publishData()
+            .tryMap{ response in
+                if response.error != nil {
+                    throw response.error!
+                }
+                return ()
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    func reportPost(boardId: Int) -> AnyPublisher<Void, Error> {
+        let url = "\(baseURL)/api/boards/report"
+        
+        let parameters: [String: Any] = [
+            "boardId": boardId
+        ]
+        
+        return session.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default)
+            .validate()
+            .publishData()
+            .tryMap{ response in
+                if response.error != nil {
+                    throw response.error!
+                }
+                return ()
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    func deletePost(boardId: Int) -> AnyPublisher<Void, Error> {
+        let url = "\(baseURL)/api/boards/\(boardId)"
+        
+        let parameters: [String: Any] = [
+            "boardId": boardId
+        ]
+        
+        return session.request(url, method: .delete, parameters: parameters)
+            .validate()
+            .publishData()
+            .tryMap{ response in
+                if response.error != nil {
+                    throw response.error!
+                }
+                return ()
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    // 게시글 수정
+    func updatePost(boardId: Int, board: PostModel) -> AnyPublisher<PostResponse, Error> {
+        let url = "\(baseURL)/api/boards/\(boardId)"
+        
+        return session.request(url, method: .put, parameters: board, encoder: URLEncodedFormParameterEncoder.default)
+            .validate()
+            .publishDecodable(type: PostResponse.self)
+            .value()
+            .mapError { $0 as Error }
+            .eraseToAnyPublisher()
+    }
 }
