f248a05defc350baaa9753815db56192a5c71678
devpark435 <devpark435@gmail.com>
2024-07-21 22:34:27 +0900

    feat #141 - Add Comment List VC UI Update

--- Sikmogil/Presentation/Community/View/Board/View/CommentListViewController.swift
+++ Sikmogil/Presentation/Community/View/Board/View/CommentListViewController.swift
@@ -5,4 +5,78 @@
 //  Created by 박현렬 on 7/21/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
+import FloatingPanel
+import Combine
+
+class CommentListViewController: UIViewController {
+    private let viewModel = CommentViewModel()
+    private var cancellables = Set<AnyCancellable>()
+    
+    private lazy var tableView: UITableView = {
+        let tv = UITableView()
+        tv.register(CommentTableViewCell.self, forCellReuseIdentifier: "CommentCell")
+        tv.delegate = self
+        tv.dataSource = self
+        tv.tableHeaderView = headerView
+        return tv
+    }()
+    
+    private lazy var headerView: UIView = {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 80))
+        let titleLabel = UILabel()
+        titleLabel.text = "댓글"
+        titleLabel.font = Suite.bold.of(size: 24)
+        view.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints {
+            $0.bottom.equalToSuperview()
+            $0.leading.equalToSuperview().offset(16)
+            $0.centerY.equalToSuperview()
+        }
+        return view
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        setupConstraints()
+        setupBindings()
+        viewModel.fetchComments()
+    }
+    
+    private func setupViews() {
+        view.addSubview(tableView)
+    }
+    
+    private func setupConstraints() {
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    private func setupBindings() {
+        viewModel.$comments
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] _ in
+                self?.tableView.reloadData()
+            }
+            .store(in: &cancellables)
+    }
+}
+
+extension CommentListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.comments.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell", for: indexPath) as? CommentTableViewCell else {
+            return UITableViewCell()
+        }
+        let comment = viewModel.comments[indexPath.row]
+        cell.configure(with: comment)
+        return cell
+    }
+}
